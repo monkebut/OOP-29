af25330d9c3307451b4a577ced77fe5ba901bb57
monkebut <166732006+monkebut@users.noreply.github.com>
2024-06-20 02:28:03 +0300

    Add files via upload

---
+++ OOP 29/OOP_29/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace OOP_29
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(186, 10);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(311, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(11, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введіть нікнейм";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(515, 3);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Вхід";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(515, 56);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Вихід";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.richTextBox1.Location = new System.Drawing.Point(9, 37);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(488, 272);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(399, 326);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Відправити";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(40, 329);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(355, 23);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(5, 327);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(31, 28);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 367);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "OOP 29";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ OOP 29/OOP_29/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace OOP_29
+{
+    public partial class Form1 : Form
+    {
+        const int LOCALPORT = 8001; // порт для приймання повідомлень
+        const int REMOTEPORT = 8001; // порт для передавання повідомлень
+        const int TTL = 20; // Time to Live для multicast
+        const string HOST = "235.5.5.1"; // адреса хоста для multicast
+
+        IPAddress groupAddress; // адреса групи для multicast
+        string nickName; // ім’я користувача в чаті
+        bool alive = false; // прапорець для перевірки активності підключення
+        UdpClient client; // клієнт для UDP-з'єднання
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Enabled = true; // увімкнення кнопки входу
+            button2.Enabled = false; // вимкнення кнопки виходу
+            button3.Enabled = false; // вимкнення кнопки відправки повідомлень
+            richTextBox1.ReadOnly = true; // поле для повідомлень доступне лише для читання
+            groupAddress = IPAddress.Parse(HOST); // парсинг адреси хоста
+        }
+
+        private void ReceiveMessages()
+        {
+            alive = true; // встановлення прапорця активності підключення
+            try
+            {
+                while (alive)
+                {
+                    IPEndPoint remoteIp = null;
+                    byte[] data = client.Receive(ref remoteIp); // приймання даних
+                    string message = Encoding.Unicode.GetString(data); // декодування повідомлення
+
+                    // додавання отриманого повідомлення в текстове поле
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        string time = DateTime.Now.ToShortTimeString();
+                        richTextBox1.Text = time + " " + message + "\r\n" + richTextBox1.Text;
+                    }));
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                if (!alive) return; // вихід, якщо підключення неактивне
+                throw;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); // виведення повідомлення про помилку
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nickName = textBox1.Text; // збереження введеного імені користувача
+            textBox1.ReadOnly = true; // поле для введення імені стає доступним лише для читання
+            try
+            {
+                client = new UdpClient(LOCALPORT); // ініціалізація UDP-клієнта з локальним портом
+                client.JoinMulticastGroup(groupAddress, TTL); // під'єднання до групового розсилання
+
+                // задача на приймання повідомлень
+                Task receiveTask = new Task(ReceiveMessages);
+                receiveTask.Start(); // запуск задачі
+
+                // перше повідомлення про вхід нового користувача
+                string message = nickName + " увійшов до чату";
+                byte[] data = Encoding.Unicode.GetBytes(message); // кодування повідомлення
+                client.Send(data, data.Length, HOST, REMOTEPORT); // відправлення повідомлення
+                button1.Enabled = false; // вимкнення кнопки входу
+                button2.Enabled = true; // увімкнення кнопки виходу
+                button3.Enabled = true; // увімкнення кнопки відправки повідомлень
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); // виведення повідомлення про помилку
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string message = String.Format("{0}: {1}", nickName, textBox2.Text); // форматування повідомлення
+                byte[] data = Encoding.Unicode.GetBytes(message); // кодування повідомлення
+                client.Send(data, data.Length, HOST, REMOTEPORT); // відправлення повідомлення
+                textBox2.Clear(); // очищення поля введення повідомлення
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); // виведення повідомлення про помилку
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string message = nickName + " покидає чат"; // повідомлення про вихід користувача
+            byte[] data = Encoding.Unicode.GetBytes(message); // кодування повідомлення
+            client.Send(data, data.Length, HOST, REMOTEPORT); // відправлення повідомлення
+            client.DropMulticastGroup(groupAddress); // вихід з групового розсилання
+            alive = false; // встановлення прапорця активності підключення в false
+            client.Close(); // закриття клієнта
+            button1.Enabled = true; // увімкнення кнопки входу
+            button2.Enabled = false; // вимкнення кнопки виходу
+            button3.Enabled = false; // вимкнення кнопки відправки повідомлень
+            textBox1.ReadOnly = false; // поле для введення імені стає доступним для редагування
+            textBox1.Clear(); // очищення поля введення імені
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (alive)
+            {
+                string message = nickName + " покидає чат"; // повідомлення про вихід користувача
+                byte[] data = Encoding.Unicode.GetBytes(message); // кодування повідомлення
+                client.Send(data, data.Length, HOST, REMOTEPORT); // відправлення повідомлення
+                client.DropMulticastGroup(groupAddress); // вихід з групового розсилання
+                alive = false; // встановлення прапорця активності підключення в false
+                client.Close(); // закриття клієнта
+                button1.Enabled = true; // увімкнення кнопки входу
+                button2.Enabled = false; // вимкнення кнопки виходу
+                button3.Enabled = false; // вимкнення кнопки відправки повідомлень
+                richTextBox1.ReadOnly = false; // поле для повідомлень стає доступним для редагування
+                richTextBox1.Clear(); // очищення поля для повідомлень
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog(); // ініціалізація діалогового вікна вибору кольору
+            dialog.ShowDialog(); // показ діалогового вікна
+            richTextBox1.ForeColor = dialog.Color; // встановлення обраного кольору для тексту
+        }
+    }
+}
